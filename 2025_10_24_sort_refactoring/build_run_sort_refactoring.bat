fe3a202d5a68d8fb9ef291e0d83c74a4e82f85be
lenor704 <elenau764@gmail.com>
2025-10-24 15:53:53 +0300

    Update build_run_sort_refactoring.bat

--- 2025_10_24_sort_refactoring/build_run_sort_refactoring.bat
+++ 2025_10_24_sort_refactoring/build_run_sort_refactoring.bat
@@ -3,7 +3,7 @@ chcp 1251 > log
 del log
 
 set CPP_FILES="io.cpp sortings.cpp main.cpp"
-set EXE=bubble_sort.exe
+set EXE=shell_sort.exe
 set CHARSET="-finput-charset=utf-8 -fexec-charset=windows-1251"
 
 if exist %EXE% del %EXE%
