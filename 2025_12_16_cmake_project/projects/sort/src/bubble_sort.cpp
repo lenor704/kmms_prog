efc8d9faa70588e4559882d885c98289af4c4e12
lenor704 <elenau764@gmail.com>
2025-12-17 15:12:51 +0300

    Google test and CMake

--- 2025_12_16_cmake_project/projects/sort/src/bubble_sort.cpp
+++ 2025_12_16_cmake_project/projects/sort/src/bubble_sort.cpp
@@ -5,7 +5,7 @@ void ule::bubble_sort(std::vector<int>& arr) {
 	for(int i = 0; i < n - 1; i++) {
 		for (int j = 0; j < n - i - 1; j++) {
 			if (arr[j] > arr[j + 1]) {
-				std::swap(arr[j], arr[i]);
+				std::swap(arr[j], arr[j + 1]);
 			}
 		}
 	}
