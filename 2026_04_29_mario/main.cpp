056cd9cff9847ddcd2812abb9d42f65ec69c642b
lenor704 <elenau764@gmail.com>
2026-05-01 11:34:40 +0300

    add object o

--- 2026_04_29_mario/main.cpp
+++ 2026_04_29_mario/main.cpp
@@ -17,8 +17,13 @@ typedef struct SObject {
 
 char map[mapHeight][mapWidth + 1];
 TObject mario;
+
 TObject *brick = NULL;
 int brickLength;
+
+TObject *moving = NULL;
+int movingLength;
+
 int level = 1;
 
 
@@ -96,8 +101,6 @@ void PutObjectOnMap(TObject obj) {
 			}
 		}
 	}
-	
-	//map[iy][ix] = '@';
 }
 
 void setCur(int x, int y) {
@@ -140,6 +143,9 @@ void CreateLevel(int lvl) {
 		InitObject(brick + 3, 120, 15, 10, 10, '#');
 		InitObject(brick + 4, 150, 20, 40, 5, '#');
 		InitObject(brick + 5, 210, 15, 10, 10, '+');
+		movingLength = 1;
+		moving = (TObject*)realloc(moving, sizeof(*moving) * movingLength);
+		InitObject(moving + 0, 25, 10, 3, 2, 'o');
 	}
 	if (lvl == 2) {
 		brickLength = 4;
@@ -177,6 +183,9 @@ int main() {
 		VertMoveObject(&mario);
 		for (int i = 0; i < brickLength; i++) {
 			PutObjectOnMap(brick[i]);
+		}
+		for (int i = 0; i < movingLength; i++) {
+			PutObjectOnMap(moving[i]);
 		}
 		PutObjectOnMap(mario);
 		
